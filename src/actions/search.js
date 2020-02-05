04a64590c563b3b8cb445ec07722d77730ae67b2
Anush Avagyan <57200991+AnushAvagyan@users.noreply.github.com>
2020-02-04 18:50:44 -0800

    Complete defining action creators

--- src/actions/search.js
+++ src/actions/search.js
@@ -4,9 +4,39 @@ import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
 
+
+
 var handleVideoSearch = (q) => {
- 
+
   //TODO:  Write an asynchronous action to handle a video search!
+// return {
+//   type: '',
+//   videos: q
+//   }
+
+var obj = {
+  //type: 'handleVideoSearch'
+}
+
+
+var options = {
+  key: YOUTUBE_API_KEY,
+  query: q
 };
+
+searchYouTube(options, (videos) => {
+  console.log('videos', videos);
+
+  obj.video = changeVideo(videos[0]);
+  obj.videos = changeVideoList(videos);
+
+  }
+ );
+
+ return function(){
+   return obj;
+ };
+
+}
 
 export default handleVideoSearch;
