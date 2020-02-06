ce51ce7da42f4cf49235229330c0aeaaba9e8428
Anush Avagyan <57200991+AnushAvagyan@users.noreply.github.com>
2020-02-06 11:20:23 -0800

    Complete replacing components with containers

--- src/containers/VideoListContainer.js
+++ src/containers/VideoListContainer.js
@@ -2,9 +2,26 @@ import { connect } from 'react-redux';
 import VideoList from './../components/VideoList.js';
 import changeVideo from './../actions/currentVideo.js';
 
-var VideoListContainer = () => {};
+//var VideoListContainer = () => {};
 
 //TODO: define a VideoListContainer component which will hook up your action
 // dispatchers with your VideoList component props.
+
+const mapStateToProps = (state) => {
+  return {
+    handleVideoListEntryTitleClick: state.handleVideoListEntryTitleClick,
+    videos: state.videoList
+
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    // dispatching plain actions
+    handleVideoListEntryTitleClick: () => dispatch({ type: 'CHANGE_VIDEO_LIST' })
+  }
+}
+
+var VideoListContainer = connect(mapStateToProps, mapDispatchToProps)(VideoList)
 
 export default VideoListContainer;
--- src/containers/VideoPlayerContainer.js
+++ src/containers/VideoPlayerContainer.js
@@ -1,9 +1,26 @@
 import { connect } from 'react-redux';
 import VideoPlayer from '../components/VideoPlayer.js';
 
-var VideoPlayerContainer = () => {};
+//var VideoPlayerContainer = () => {};
 
 //TODO: define a VideoPlayerContainer component which will hook up your action
 //dispatchers with your VideoPlayer component props.
+
+const mapStateToProps = (state) => {
+  return {
+
+    video: state.currentVideo
+
+  }
+}
+
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     // dispatching plain actions
+
+//   }
+// }
+
+var VideoPlayerContainer = connect(mapStateToProps)(VideoPlayer)
 
 export default VideoPlayerContainer;
